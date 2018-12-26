b0b66c4fb66523de905e01646dc2274c516a184d
cagdaseksi <cagdas.eksi@hotmail.com>
2018-12-26 11:15:11 +0300

    Initial Commit

---
+++ ShowPlaceOnMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowPlaceOnMap
+//
+//  Created by MAC on 26/12/18.
+//  Copyright © 2018 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
